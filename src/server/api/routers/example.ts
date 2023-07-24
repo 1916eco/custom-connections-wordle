af1aac629b64a38527f40a86c6c29efb18782217
1916eco <1916eco@gmail.com>
2023-07-24 13:31:35 +0100

    Ui Updated

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -2,14 +2,16 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
+  getGame: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.gameWords.findUnique({
+        where: {
+          id: input.id,
+        },
+        include: {
+          words: true,
+        },
+      });
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
 });
